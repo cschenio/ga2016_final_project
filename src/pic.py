b218c744408705cecb41cc8032e9747caed131b2
b03902128 <b03902128@ntu.edu.tw>
2016-12-22 20:39:35 +0800

    Add files via upload

---
+++ src/pic.py
@@ -0,0 +1,34 @@
+from PIL import Image, ImageDraw
+from sys import argv
+import numpy as np
+
+def pic2rgb(filename):
+
+    width = 400
+    height = 400
+    new_pic = "resized.jpg"
+    
+    im = Image.open(filename)
+    print ('input pic: ', im.format, im.size, im.mode)
+    if im.mode!='RGB': 
+        print('Please use RGB format picture.')
+        quit()
+
+
+    nim2 = im.resize( (width, height), Image.BILINEAR )
+    nim2.save(new_pic)
+    nim2 = Image.open(new_pic)
+    print ('conver to: ', nim2.format, nim2.size, nim2.mode)
+    print('save as: %s'%new_pic, '\n')
+
+    im_array=np.asarray(nim2)
+    print('numpy array shape: ',im_array.shape)
+    #print('Example: the first row','\n')
+    #print(im_array)
+    return im_array
+
+def rgb2pic(im_array):
+    ans_pic = "master_piece.jpg"
+    im = Image.fromarray(im_array)
+    im.save(ans_pic)
+    print('save GA as: ',ans_pic)
