a9736ec2fed35fa2b20ec1c8eacf4264fe5dcc32
b03902128 <b03902128@ntu.edu.tw>
2016-12-25 17:16:26 +0800

    Update pic.py

--- src/pic.py
+++ src/pic.py
@@ -1,8 +1,14 @@
 from PIL import Image, ImageDraw
 from sys import argv
 import numpy as np
+from colormath.color_objects import sRGBColor, LabColor, XYZColor
+from colormath.color_diff import delta_e_cie2000
+from colormath.color_conversions import convert_color
 
-def pic2rgb(filename, width=400, height=400):
+def pic2rgb(filename):
+
+    width = 100
+    height = 100
     new_pic = "resized.jpg"
     
     im = Image.open(filename)
@@ -17,12 +23,18 @@ def pic2rgb(filename, width=400, height=400):
     nim2 = Image.open(new_pic)
     print ('conver to: ', nim2.format, nim2.size, nim2.mode)
     print('save as: %s'%new_pic, '\n')
+    
+    nim2=np.asarray(nim2)
+    row, col = len(nim2), len(nim2[0])
+    labcolor_img=[]
+    
+    for i in range(row):
+        for j in range(col):
+            labcolor_img.append(convert_color(sRGBColor(nim2[i][j][0], nim2[i][j][1], nim2[i][j][2], True), LabColor))
+        
+    labcolor_img = np.array(labcolor_img).reshape(row, col)
 
-    im_array=np.asarray(nim2)
-    print('numpy array shape: ',im_array.shape)
-    #print('Example: the first row','\n')
-    #print(im_array)
-    return im_array
+    return labcolor_img
 
 def rgb2pic(im_array, filename):
     ans_pic = filename
