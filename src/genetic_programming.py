61689f49d27f8f568d69f38bc47e54c2bd8c4802
sean128309 <b02502038@ntu.edu.tw>
2016-12-28 17:08:24 +0800

    奧步

--- src/genetic_programming.py
+++ src/genetic_programming.py
@@ -16,28 +16,6 @@ class cut:
     self.name = name
     self.childcount = 2
 
-class variable:
-  def __init__(self, var, value=0):
-    self.var = var
-    self.value = value
-    self.name = str(var)
-    self.type = "variable"  
-
-  def evaluate(self):
-    return self.value
-
-  def setvar(self, value):
-    self.value = value
-
-  def display(self, indent=0):
-    print (self.var)
-
-class const:
-  def __init__(self, value):
-    self.value = value
-    self.name = str(value)
-    self.type = "constant"   
-
   def evaluate(self):
     return self.value
 
@@ -45,7 +23,7 @@ class const:
     print (self.value)
 
 class node:
-  def __init__(self, type, children, funwrap, var=None, const=None):
+  def __init__(self, type, children, funwrap, var="", const=None):
     self.type = type
     self.children = children
     self.funwrap = funwrap
@@ -61,35 +39,13 @@ class node:
     self.lisporder = ""
     self.matrix = None
 
-  def eval(self):
-    if self.type == "variable":
-      return self.variable.value
-    elif self.type == "constant":
-      return self.const.value
-    else:
-      for c in self.children:
-        result = [c.eval() for c in self.children]
-      return self.funwrap.function(result)  
 
   def getfitness(self, image):
-    (x, y, _) = image.shape
+    x, y  = image.shape
     self.matrix = cf.to_array(self.display(), x, y, 1)
     self.fitness = cf.cost(self.matrix, image)
 #    self.getcut()
-#    self.fitness=10-self.cut
-
-  def setvariablevalue(self, value):
-    if self.type == "variable":
-      if value.has_key(self.variable.var):
-        self.variable.setvar(value[self.variable.var])
-      else:
-        print ("There is no value for variable:", self.variable.var)
-        return
-    if self.type == "constant":
-      pass
-    if self.children:#function node
-      for child in self.children:
-        child.setvariablevalue(value)            
+#    self.fitness=10-self.cut       
 
   def refreshdepth(self):
     self.lisporder = ""
@@ -111,7 +67,7 @@ class node:
     if self.type == "function":
       self.lisporder += (self.funwrap.name + " " + str(self.portion) + " ")
     elif self.type == "variable":
-      self.lisporder += self.variable.name
+      self.lisporder += self.variable
     if self.children:
       for c in self.children:
         self.lisporder += c.display(indent + 1)
@@ -125,16 +81,6 @@ class node:
       for c in self.children:
         self.cut += c.getcut(indent + 1)
     return self.cut
-  ##for draw node
-  def getwidth(self):
-    if self.type == "variable" or self.type == "constant":
-      return 1
-    else:
-      result = 0
-      for i in range(0, len(self.children)):
-        result += self.children[i].getwidth()
-      return result
-    
 
 class enviroment:
   def __init__(self, funwraplist, variablelist, constantlist, checkdata, target_image,\
@@ -180,62 +126,51 @@ class enviroment:
           temp.append(Tree)
           break
     return temp
-  def _maketree(self, startdepth):
-    if startdepth == 0:
-      #make a new tree
-      nodepattern = 0#function
+  def _maketree(self, startdepth, threshold=0.5):
+    if startdepth < 2:
+      nodepattern = 0
     elif startdepth == self.maxdepth:
       nodepattern = 1#variable or constant
     else:
-      nodepattern = randint(0, 1)
-    if nodepattern == 0:
+      nodepattern = random()
+    if nodepattern <= threshold:
       childlist = []
       selectedfun = randint(0, len(self.funwraplist) - 1)
       for i in range(0, self.funwraplist[selectedfun].childcount):
-        child = self._maketree(startdepth + 1)
+        child = self._maketree(startdepth + 1, threshold)
         childlist.append(child)
       return node("function", childlist, self.funwraplist[selectedfun])
     else:
       selectedvariable = randint(0, len(self.variablelist) - 1)
       return node("variable", None, None, \
-             variable(self.variablelist[selectedvariable]), None)
+            self.variablelist[selectedvariable], None)
 
-  def mutate(self, tree, probchange=0.5, startdepth=0):
+  def mutate(self, tree, probchange=0.3, startdepth=0):
     if random() < probchange:
-      if tree.type == "function":
-        selectedfun = randint(0, len(self.funwraplist) - 1)
-        tree.funwrap = self.funwraplist[selectedfun]
-        tree.portion = round(random(), 3)
-      else:
-        if random() < 0.5:
-          selectedvariable = randint(0, len(self.variablelist) - 1)
-          tree.variable = self.variablelist[selectedvariable]
-        else:
-          tree.type = "function"
-          selectedfun = randint(0, len(self.funwraplist) - 1)
-          tree.funwrap = self.funwraplist[selectedfun]
-          tree.portion = round(random(), 3)
-          tree.children = [self._maketree(self.maxdepth) for i in range(0,2)]
+      tree = self._maketree(2)
     elif tree.type == "function":
       selectedchild = randint(0, len(tree.children) - 1)
       self.mutate(tree.children[selectedchild], probchange, startdepth + 1)
 
-  def crossover(self, tree1, tree2, probswap=0.8, top=1):
-    if not top:
-      return deepcopy(tree2)
-    else:
-      result = deepcopy(tree1)
-      if tree1.type == "function" and tree2.type == "function":
-        result.children = [self.crossover(c, choice(tree2.children), \
-                           probswap, 0) for c in tree1.children]
-    return result
+
+  def crossover(self, tree1, tree2, probnext=0.8, top=1):
+    if random() > probnext or tree1.type == "variable":
+      tree1 = tree2
+    else :
+      self.crossover(choice(tree1.children),tree2)
+
+  def getsubtree(self, tree, probnext = 0.8):
+    if random() > probnext or tree.type == "variable":
+      return tree
+    else :
+      return self.getsubtree(choice(tree.children))
 
   def envolve(self, maxgen=100, crossrate=0.9, mutationrate=0.1):
     for i in range(0, maxgen):
       print ("generation no.", i)
       self.listpopulation()
-      for j in range(0, self.size):
-        print (self.population[j].lisporder)
+      #for j in range(0, self.size):
+        #print (self.population[j].lisporder)
       self.nextgeneration = []
       for j in range(0, self.size):
         self.nextgeneration.append(self.population[j])
@@ -246,7 +181,9 @@ class enviroment:
           while True:
             parent1 = self.population[int(random() * (self.size))]
             parent2 = self.population[int(random() * (self.size))]
-            child = self.crossover(parent1, parent2)
+            child = deepcopy(parent1)
+            sub = self.getsubtree(parent2)
+            self.crossover(child,sub)
             child.refreshdepth()
             if child.getcut() <= self.maxcut:
               self.nextgeneration.append(child)
@@ -291,6 +228,7 @@ class enviroment:
         randomnum += dis
         if randomnum >= float(len(self.nextgeneration) - 1):
           randomnum -= float(len(self.nextgeneration) - 1)
+      print (self.besttree.lisporder)
       if self.besttree.fitness == 0:
         break;
     return self.besttree.lisporder
